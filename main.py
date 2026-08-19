c3d735fe422e8e7b0f0a56abba56d18f64140205
Soumyapro <84585023+Soumyapro@users.noreply.github.com>
2026-08-19 01:29:48 +0100

    Change model provider from 'qwen' to 'openai'

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ prompt2 = ChatPromptTemplate.from_messages([
 # initialize model 1
 first_llm_model = init_chat_model(
     model_provider="groq",
-    model="qwen/qwen3.6-27b"
+    model="openai/gpt-oss-safeguard-20b"
 )
 
 # initialize model 2
